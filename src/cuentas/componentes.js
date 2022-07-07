0780655a5ea6b9ef02ae4f4c0de62cb30357d3a9
cristianlhernandez <cristianlhernandez@hotmail.com>
2022-07-07 15:57:14 -0300

    Formulario Cuentas

--- src/cuentas/componentes.js
+++ src/cuentas/componentes.js
@@ -1,7 +1,97 @@
 /* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
 $(function () {
-  $("#run").dxButton({
-    text: "Click",
-    stylingMode: "outlined",
+  $("#form")
+    .dxForm({
+      formData: dato,
+      labelMode: "floating",
+      items: [
+        {
+          dataField: "codigoCuenta",
+          isRequired: "true",
+          editorType: "dxTextBox",
+          label: {
+            text: "Código de la Cuenta",
+          },
+          editorOptions: {
+            showClearButton: true,
+            mask: "0.0.00.00/000",
+            maskChar: "-",
+          },
+        },
+        {
+          dataField: "nombreCuenta",
+          editorType: "dxTextBox",
+          isRequired: "true",
+          label: {
+            text: "Nombre de la Cuenta",
+          },
+          editorOptions: {
+            showClearButton: true,
+          },
+        },
+        {
+          dataField: "descripcionCuenta",
+          editorType: "dxTextArea",
+          label: {
+            text: "Descripción de la Cuenta",
+          },
+          editorOptions: {
+            showClearButton: true,
+          },
+        },
+        {
+          itemType: "button",
+          buttonOptions: {
+            text: "Guardar",
+            stylingMode: "outlined",
+            type: "default",
+            useSubmitBehavior: true,
+          },
+        },
+      ],
+    })
+    .dxForm("instance");
+
+  $("#form-container").on("submit", async function (e) {
+    e.preventDefault();
+    console.log(dato.codigoCuenta);
+    try {
+      await Excel.run(async (context) => {
+        let sheetCuentas = context.workbook.worksheets.getItemOrNullObject("Plan de Cuentas");
+        await context.sync();
+        // eslint-disable-next-line office-addins/load-object-before-read
+        if (sheetCuentas.isNullObject) {
+          sheetCuentas = context.workbook.worksheets.add("Plan de Cuentas");
+          //Creat la tabla.
+          let tablaCuentas = sheetCuentas.tables.add("A1:C1", true);
+          //Establecer el nombre de la tabla.
+          tablaCuentas.name = "PlanCuentas";
+          //Colocar las cabeceras de la tabla.
+          tablaCuentas.getHeaderRowRange().values = [["Código", "Cuenta", "Descripción"]];
+          //tablaCuentas.columns.getItemAt(0).getDataBodyRange().numberFormat = [["#-#-##-##-###"]];
+          sheetCuentas.getUsedRange().format.autofitColumns();
+          sheetCuentas.getUsedRange().format.autofitRows();
+        } else {
+          let tablaCuentas = sheetCuentas.tables.getItem("PlanCuentas");
+          await context.sync();
+          let cuenta = [dato.codigoCuenta, dato.nombreCuenta, dato.descripcionCuenta];
+          tablaCuentas.rows.add(null, [cuenta], true);
+
+          dato.values = null;
+        }
+        await context.sync();
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
+
+const dato = [
+  {
+    codigoCuenta: "",
+    nombreCuenta: "",
+    descripcionCuenta: "",
+  },
+];
--- src/cuentas/cuentas.js
+++ src/cuentas/cuentas.js
@@ -1,35 +1,28 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global console, document, Excel, Office */
-
+/* eslint-disable no-undef */
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
-    document.getElementById("run").onclick = run;
     console.log("Running Excel");
   }
 });
 
-async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
+// async function run() {
+//   try {
+//     await Excel.run(async (context) => {
+//       /**
+//        * Insert your Excel code here
+//        */
+//       const range = context.workbook.getSelectedRange();
 
-      // Read the range address
-      range.load("address");
+//       // Read the range address
+//       range.load("address");
 
-      // Update the fill color
-      range.format.fill.color = "yellow";
+//       // Update the fill color
+//       range.format.fill.color = "yellow";
 
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+//       await context.sync();
+//       console.log(`The range address was ${range.address}.`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
